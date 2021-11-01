f686c100299253144c9242f119227743a8dd1080
watermahlonz <mahalon.devoe@gmail.com>
2021-11-02 00:01:10 +1300

    no message

--- 2D_TOPDOWN/Week_1/main.cpp
+++ 2D_TOPDOWN/Week_1/main.cpp
@@ -2,80 +2,79 @@
 #include <iostream>
 #include "Cards.h"
 #include "Button.h"
-#include "Game.h"
 
 // the abstract base class
-class Node
-{
-public:
-
-    // ... functions to transform the node
-    Node()
-    {
-
-    }
-
-    void setTransform(sf::Transform transform)
-    {
-        m_transform = transform;
-    }
-
-    sf::Transform getTransform()
-    {
-        return m_transform;
-    }
-
-    // ... functions to manage the node's children
-    void addChild(Node* child)
-    {
-        m_children.push_back(child);
-    }
-
-    void draw(sf::RenderTarget& target, const sf::Transform& parentTransform) const
-    {
-        // combine the parent transform with the node's one
-        sf::Transform combinedTransform = parentTransform * m_transform;
-
-        // let the node draw itself
-        onDraw(target, combinedTransform);
-
-        // draw its children
-        for (std::size_t i = 0; i < m_children.size(); ++i)
-        {
-            m_children[i]->draw(target, combinedTransform);
-        }
-
-    }
-
-private:
-
-    virtual void onDraw(sf::RenderTarget& target, const sf::Transform& transform) const = 0;
-
-    sf::Transform m_transform;
-    std::vector<Node*> m_children;
-};
-
-// a simple derived class: a node that draws a sprite
-class SpriteNode : public Node
-{
-public:
-    // .. functions to define the sprite
-    SpriteNode(sf::Transform transform)
-    {
-        setTransform(transform);
-        m_sprite.setFillColor(sf::Color::Red);
-        m_sprite.setSize(sf::Vector2f(10.f, 10.f));
-    }
-
-private:
-
-    virtual void onDraw(sf::RenderTarget& target, const sf::Transform& transform) const
-    {
-        target.draw(m_sprite, transform);
-    }
-
-    sf::RectangleShape m_sprite;
-};
+//class Node
+//{
+//public:
+//
+//    // ... functions to transform the node
+//    Node()
+//    {
+//
+//    }
+//
+//    void setTransform(sf::Transform transform)
+//    {
+//        m_transform = transform;
+//    }
+//
+//    sf::Transform getTransform()
+//    {
+//        return m_transform;
+//    }
+//
+//    // ... functions to manage the node's children
+//    void addChild(Node* child)
+//    {
+//        m_children.push_back(child);
+//    }
+//
+//    void draw(sf::RenderTarget& target, const sf::Transform& parentTransform) const
+//    {
+//        // combine the parent transform with the node's one
+//        sf::Transform combinedTransform = parentTransform * m_transform;
+//
+//        // let the node draw itself
+//        onDraw(target, combinedTransform);
+//
+//        // draw its children
+//        for (std::size_t i = 0; i < m_children.size(); ++i)
+//        {
+//            m_children[i]->draw(target, combinedTransform);
+//        }
+//
+//    }
+//
+//private:
+//
+//    virtual void onDraw(sf::RenderTarget& target, const sf::Transform& transform) const = 0;
+//
+//    sf::Transform m_transform;
+//    std::vector<Node*> m_children;
+//};
+//
+//// a simple derived class: a node that draws a sprite
+//class SpriteNode : public Node
+//{
+//public:
+//    // .. functions to define the sprite
+//    SpriteNode(sf::Transform transform)
+//    {
+//        setTransform(transform);
+//        m_sprite.setFillColor(sf::Color::Red);
+//        m_sprite.setSize(sf::Vector2f(10.f, 10.f));
+//    }
+//
+//private:
+//
+//    virtual void onDraw(sf::RenderTarget& target, const sf::Transform& transform) const
+//    {
+//        target.draw(m_sprite, transform);
+//    }
+//
+//    sf::RectangleShape m_sprite;
+//};
 
 int main()
 {
